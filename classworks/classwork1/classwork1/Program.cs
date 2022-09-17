064914be3aaa6065518f5c333001008d7dbcfdd2
Андрей Рубан <anruban2001@gmail.com>
2022-09-17 17:52:36 +0300

    classwork1

---
+++ classworks/classwork1/classwork1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i=u/r
+            //r- омы
+            //u - вольт
+            //i - ампери
+            bool isOK = false;
+            do
+            {
+                Console.WriteLine("Введіть напругу:");
+                string strU = Console.ReadLine();
+                isOK = double.TryParse(strU, out U);
+                if (!isOK)
+                {
+                    Console.WriteLine("Погане слово");
+                }
+            }
+            while (!isOK);
+            Console.WriteLine("Введіть опір:");
+            double strR = double.Parse(Console.ReadLine());
+            if (strR <= 0)
+            {
+                Console.WriteLine("Теж погане слово!");
+                Console.ReadLine();
+                return;
+            }
+            I = U / strR;
+            Console.WriteLine($"Держи {I}");
+            Console.ReadKey();
+        }
+    }
+}
