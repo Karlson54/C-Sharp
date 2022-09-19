fc249ca1e7e45ce3e7a911774cb981b0fb4d0b49
Андрей Рубан <anruban2001@gmail.com>
2022-09-19 16:19:20 +0300

    lab3

---
+++ labs/lab3/lab3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace lab3
+{
+    class lab3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11 задание");
+        again:
+            string a1 = Console.ReadLine();
+            bool isCorrect = false;
+            if (check(a1))
+            {
+
+                try
+                {
+                    int c1 = int.Parse(a1);
+                    string b1 = a1.ToString();
+                    char[] num = b1.ToCharArray();
+                    Array.Reverse(num);
+                    string b2 = new string(num);
+                    int a2 = int.Parse(b2);
+                    if (c1 == a2)
+                    {
+                        isCorrect = true;
+                    }
+                    Console.WriteLine(isCorrect);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Цифры!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("не больше 4!!!");
+                goto again;
+            }
+
+            Console.ReadKey();
+        }
+        bool check (string a1)
+        {
+            string a2 = a1;
+            bool check = false;
+            if (a2.Length == 4) 
+            {
+                check = true;
+            }
+            return check;
+        }
+    }
+}
