b201494f15ab92056e3e9d8e0906935ea190011e
Андрей Рубан <anruban2001@gmail.com>
2022-09-25 00:50:56 +0300

    deleting GOTO

--- labs/lab3/lab3/Program.cs
+++ labs/lab3/lab3/Program.cs
@@ -1,42 +1,31 @@
 ﻿using System;
+using check;
 
 namespace lab3
 {
     class lab3
     {
+        static string Check1(out string b)
+        {
+            try
+            {
+                Console.WriteLine("Введите число");
+                int a = int.Parse(Console.ReadLine());
+                b = a.ToString();
+                return b;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Цифры!!!");
+                return Check1(out b);
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("11 задание");
-        again:
-            string a1 = Console.ReadLine();
-            bool isCorrect = false;
-            if (a1.Length <= 4)
-            {
-
-                try
-                {
-                    int c1 = int.Parse(a1);
-                    string b1 = a1.ToString();
-                    char[] num = b1.ToCharArray();
-                    Array.Reverse(num);
-                    string b2 = new string(num);
-                    int a2 = int.Parse(b2);
-                    if (c1 == a2)
-                    {
-                        isCorrect = true;
-                    }
-                    Console.WriteLine(isCorrect);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Цифры!!!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("не больше 4!!!");
-                goto again;
-            }
+            checks check = new checks();
+            Check1(out string b);
+            check.Check2(b);
             Console.ReadKey();
         }
     }
---
+++ labs/lab3/lab3/checks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace check
+{
+    class checks
+    {
+        public void Check2(string a)
+        {
+            bool isCorrect = false;
+            if (a.Length == 4)
+            {
+                char[] num = a.ToCharArray();
+                Array.Reverse(num);
+                int a1 = int.Parse(a);
+                string b2 = new string(num);
+                int a2 = int.Parse(b2);
+                if (a1 == a2)
+                {
+                    isCorrect = true;
+                    Console.WriteLine($"{a1} = {a2} {isCorrect}");
+                }
+                else
+                {
+                    Console.WriteLine($"{a1} != {a2} {isCorrect}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("4 цифры!!!");
+                a = Console.ReadLine();
+                Check2(a);
+            }
+        }
+    }
+}
