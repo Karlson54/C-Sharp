f32641477cc6d95d212a4c544e1b48ed8538d842
Андрей Рубан <anruban2001@gmail.com>
2022-09-30 23:44:38 +0300

    lab4 0.1

---
+++ labs/lab5/lab5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static int Check(out int countmass)
+        {
+            try
+            {
+                countmass = int.Parse(Console.ReadLine());
+                return countmass;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Цифры!!!");
+                return Check(out countmass);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            int n, m;
+            Console.WriteLine("Введите кол-во строк");
+            Check(out n);
+            Console.WriteLine("Введите кол-во столбцов");
+            Check(out m);
+            int[,] mas = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.WriteLine($"Введите {i}:{j}");
+                    mas[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
