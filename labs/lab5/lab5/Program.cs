654699ab2bd677809204996da565f71c1bb0b9d3
Андрей Рубан <anruban2001@gmail.com>
2022-10-02 18:58:49 +0300

    lab5

--- labs/lab5/lab5/Program.cs
+++ labs/lab5/lab5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lab5
 {
@@ -23,18 +19,21 @@ namespace lab5
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("20 задание");
+            Console.WriteLine("24 задание");
             int n, m;
             Console.WriteLine("Введите кол-во строк");
             Check(out n);
             Console.WriteLine("Введите кол-во столбцов");
             Check(out m);
+            int num;
+            Console.WriteLine("Какое число искать?");
+            Check(out num);
             int[,] mas = new int[n, m];
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
-                    Console.WriteLine($"Введите {i}:{j}");
+                    Console.WriteLine($"Введите {i + 1}:{j + 1}");
                     Check(out mas[i, j]);
                 }
             }
@@ -47,6 +46,32 @@ namespace lab5
                 }
                 Console.WriteLine();
             }
+
+            int minS = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (num == mas[i, j])
+                    {
+                        minS = i;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив переделанный:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (i != minS)
+                    {
+                        Console.Write(mas[i, j] + "\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
         }
     }
