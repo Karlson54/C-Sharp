68fa68f4b66902aebd5fd7a8274c3ba55325fe05
Андрей Рубан <anruban2001@gmail.com>
2022-10-10 13:45:49 +0300

    lab6

---
+++ labs/lab6/lab6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Reverse(int value)
+        {
+            string valueS = value.ToString();
+            char[] num = valueS.ToCharArray();
+            Array.Reverse(num);
+            Console.WriteLine(num);
+        }
+        static void Reverse(string value)
+        {
+            char[] word = value.ToCharArray();
+            Array.Reverse(word);
+            Console.WriteLine(word);
+        }
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(new string('-', 30));
+            int check;
+            bool succes = int.TryParse(word, out check);
+            if (succes)
+            {
+                int num = int.Parse(word);
+                Reverse(num);
+            }
+            else
+            {
+                Reverse(word);
+            }
+            Console.ReadLine();
+        }
+    }
+}
