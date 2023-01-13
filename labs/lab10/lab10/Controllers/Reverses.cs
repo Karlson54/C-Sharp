bb67c58b0a80954882a973193f17c3b450e0045f
Андрей Рубан <anruban2001@gmail.com>
2023-01-13 18:00:29 +0200

    lab10

---
+++ labs/lab10/lab10/Controllers/Reverses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace lab10.controllers
+{
+    public static class Reverses
+    {
+        public static string Reverse(this int integer)
+        {
+            string result = "";
+            while (integer != 0)
+            {
+                result += integer % 10;
+                integer /= 10;
+            }
+
+            return result;
+        }
+
+        public static string Reverse(this string str)
+        {
+            string result = "";
+            for (int i = str.Length - 1; i > -1; i--)
+            {
+                result += str[i];
+            }
+
+            return result;
+        }
+
+
+        public static string Reverse(this double doubl)
+        {
+            string[] strArr = doubl.ToString().Split(',');
+            return Reverse(strArr[0]) + "." + Reverse(strArr[1]);
+
+        }
+
+        public static string Reverse(this string str, char magicSymbol)
+        {
+            string[] strArr = str.Split(magicSymbol);
+            string result = "";
+            foreach (string i in strArr)
+            {
+                result += Reverse(i) + magicSymbol;
+            }
+
+            return result.Substring(0, result.Length - 1);
+        }
+
+        public static T[] Reverse<T>(this T[] arr)
+        {
+            T[] reverseArr = new T[arr.Length];
+            for (int j = 0, i = arr.Length - 1; i > -1; i--, j++)
+            {
+                reverseArr[j] = arr[i];
+            }
+
+
+            return reverseArr;
+        }
+    }
+}
---
+++ labs/lab10/lab10/Program.cs
@@ -0,0 +1,40 @@
+﻿using lab10.controllers;
+using System;
+
+namespace lab6
+{
+    public static class Printer
+    {
+        public static void PrintArr<T>(this T[] arr)
+        {
+            foreach (T element in arr)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+
+
+        //6 задание
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{434534.Reverse()} {21321.44.Reverse()} {"ABD".Reverse()} {"ABD,DBA".Reverse()}");
+            new int[] { 1, 2, 3, 4 }.Reverse().PrintArr();
+            new int[] { 4, 5, 32, 7, }.Sort().PrintArr();
+            Console.ReadKey();
+        }
+    }
+    public static class Extension
+    {
+        public static int[] Sort(this int[] arr)
+        {
+            Array.Sort(arr);
+            return arr;
+        }
+    }
+}
